156bbbeb324369b0fc439c650cce4c2f8c80cae5
heroin0 <black.phoenix4466@gmail.com>
2016-11-22 11:05:42 +0500

    Tweaking for target machine

--- parallel_file/Source.cpp
+++ parallel_file/Source.cpp
@@ -3,41 +3,46 @@
 #include <vector>
 #include <string>
 #include <cmath>
-#include <mpi.h>
-#include <chrono>
+//#include <mpi.h>
+//#include <chrono>
 #include <ctime>
-#include <ppl.h>
+//#include <ppl.h>
 using namespace std;
 //метод наискорейшего спуска
-using namespace std::chrono;
+//using namespace std::chrono;
 
 class GravimetryTask
 {
 private:
-	vector<vector<vector<double>>>	firstSurfacePoints, secondSurfacePoints;
-	double dx = 204.082;
-	double dy = 204.082;
-	double gravityConst = 6.6708e-3;// 6.67408e-11;
-	int gridPointsAmount = 50;
-	int sqaredGridPointsAmount = 2500;
+	vector<vector<vector<double> > >	firstSurfacePoints, secondSurfacePoints;
+	double dx;
+	double dy;
+	double gravityConst;// 6.67408e-11;
+	int gridPointsAmount;
+	int sqaredGridPointsAmount;
 public:
 	GravimetryTask(string firstSurfaceFile, string secondSurfaceFile)
 	{
+		dx = 204.082;
+		dy = 204.082;
+		gravityConst = 6.6708e-3;// 6.67408e-11;
+		gridPointsAmount = 50;
+		sqaredGridPointsAmount = 2500;
 		const int dimensions = 3;
 		readDataFile(firstSurfaceFile, dimensions, firstSurfacePoints);
 		readDataFile(secondSurfaceFile, dimensions, secondSurfacePoints);
-		vector<vector<double>> equationMatrix;
+		
+		vector<vector<double> > equationMatrix, transposedMatrix, AonATransposed, z(1, vector<double>(sqaredGridPointsAmount, 0));
 		calculateMatrix(equationMatrix);
-		vector<vector<double>> transposedMatrix;
 		transpose(equationMatrix, transposedMatrix);
-		vector<vector<double>> AonATransposed;
-		parallelMultiplicate(equationMatrix, transposedMatrix, AonATransposed);
+		//parallelMultiplicate(equationMatrix, transposedMatrix, AonATransposed);
+		multiplicate(equationMatrix, transposedMatrix, AonATransposed);
 	}
 
-	void transpose(vector<vector<double>> &inputMatrix, vector<vector<double>> &result)
+	void transpose(vector<vector<double> > &inputMatrix, vector<vector<double> > &result)
 	{
 		const int m = inputMatrix.size(), n = inputMatrix[0].size();
-		vector<vector<double>> tmpVector(m, vector<double>(n));
+		vector<vector<double> > tmpVector(m, vector<double>(n));
 		//так себе транспозиция
 		for (int i = 0; i < m; i++)
 			for (int j = 0; j < n; j++)
@@ -45,10 +50,10 @@ public:
 		swap(tmpVector, result);
 	}
 
-	void multiplicate(vector<vector<double>> &firstMatrix, vector<vector<double>> &secondMatrix, vector<vector<double>> &result)
+	void multiplicate(vector<vector<double> > &firstMatrix, vector<vector<double> > &secondMatrix, vector<vector<double> > &result)
 	{
 		const int m1 = firstMatrix.size(), m2 = secondMatrix.size(), n1 = firstMatrix[0].size(), n2 = secondMatrix[0].size();
-		vector<vector<double>> tmpMatrix(m1, vector<double>(n2, 0));
+		vector<vector<double> > tmpMatrix(m1, vector<double>(n2, 0));
 		for (int i = 0; i < m1; i++)
 			for (int j = 0; j < n2; j++)
 				for (int k = 0; k < m2; k++)
@@ -56,30 +61,30 @@ public:
 		swap(tmpMatrix, result);
 	}
 
-	void parallelMultiplicate(vector<vector<double>> &firstMatrix, vector<vector<double>> &secondMatrix, vector<vector<double>> &result)
-	{
-		const int m1 = firstMatrix.size(), m2 = secondMatrix.size(), n1 = firstMatrix[0].size(), n2 = secondMatrix[0].size();
-		vector<vector<double>> tmpMatrix(m1, vector<double>(n2, 0));
-		Concurrency::parallel_for(0, m1, [&](int i)
-		{
-			for (int j = 0; j < n2; j++)
-				for (int k = 0; k < m2; k++)
-					tmpMatrix[i][j] += firstMatrix[i][k] * secondMatrix[k][j];
-		});
-		swap(tmpMatrix, result);
-	}
+	//void parallelMultiplicate(vector<vector<double> > &firstMatrix, vector<vector<double> > &secondMatrix, vector<vector<double> > &result)
+	//{
+	//	const int m1 = firstMatrix.size(), m2 = secondMatrix.size(), n1 = firstMatrix[0].size(), n2 = secondMatrix[0].size();
+	//	vector<vector<double> > tmpMatrix(m1, vector<double>(n2, 0));
+	//	Concurrency::parallel_for(0, m1, [&](int i)
+	//	{
+	//		for (int j = 0; j < n2; j++)
+	//			for (int k = 0; k < m2; k++)
+	//				tmpMatrix[i][j] += firstMatrix[i][k] * secondMatrix[k][j];
+	//	});
+	//	swap(tmpMatrix, result);
+	//}
 
-	void subtraction(vector<vector<double>> &firstMatrix, vector<vector<double>> &secondMatrix, vector<vector<double>> &result)
+	void subtraction(vector<vector<double> > &firstMatrix, vector<vector<double> > &secondMatrix, vector<vector<double> > &result)
 	{
 		int m = firstMatrix.size(), n = firstMatrix[0].size();
-		vector<vector<double>> tmpMatrix(m, vector<double>(n, 0));
+		vector<vector<double> > tmpMatrix(m, vector<double>(n, 0));
 		for (int i = 0; i < m; i++)
 			for (int j = 0; j < n; j++)
 				tmpMatrix[i][j] = firstMatrix[i][j] - secondMatrix[i][j];
 		swap(tmpMatrix, result);
 	}
 
-	double vectorNorm(vector<vector<double>> &matrix)
+	double vectorNorm(vector<vector<double> > &matrix)
 	{
 		double result = 0;
 		for (unsigned int i = 0; i < matrix.size(); i++)
@@ -87,21 +92,21 @@ public:
 		return sqrt(result);
 	}
 
-	void multiplicate(vector<vector<double>> &matrix, double number, vector<vector<double>> &result)
+	void multiplicate(vector<vector<double> > &matrix, double number, vector<vector<double> > &result)
 	{
 		const int m = matrix.size(), n = matrix[0].size();
-		vector<vector<double>> tmpVector(m, vector<double>(n));
+		vector<vector<double> > tmpVector(m, vector<double>(n));
 		for (int i = 0; i < m; i++)
 			for (int j = 0; j < n; j++)
 				tmpVector[i][j] = matrix[i][j] * number;
 		swap(result, tmpVector);
 	}
 
-	void readDataFile(string fileName, int dimensions, vector<vector<vector<double>>> &result)//should be faster
+	void readDataFile(string fileName, int dimensions, vector<vector<vector<double> > > &result)//should be faster
 	{
-		vector<vector<vector<double>>>  tmpVector(gridPointsAmount, vector<vector<double>>(gridPointsAmount, vector<double>(dimensions)));
+		vector<vector<vector<double> > >  tmpVector(gridPointsAmount, vector<vector<double> >(gridPointsAmount, vector<double>(dimensions)));
 		ifstream  myFile;
-		myFile.open(fileName);
+		myFile.open(fileName.c_str());
 		for (int i = 0; i < gridPointsAmount; i++)
 		{
 			for (int j = 0; j < gridPointsAmount; j++)
@@ -116,9 +121,9 @@ public:
 		result.swap(tmpVector);
 	}
 
-	void calculateMatrix(vector<vector<double>> &result)
+	void calculateMatrix(vector<vector<double> > &result)
 	{
-		vector<vector<double>> tmpVector(gridPointsAmount*gridPointsAmount, vector<double>(gridPointsAmount*gridPointsAmount));
+		vector<vector<double> > tmpVector(gridPointsAmount*gridPointsAmount, vector<double>(gridPointsAmount*gridPointsAmount));
 
 		for (int k = 0; k < gridPointsAmount; k++)
 		{
@@ -147,13 +152,13 @@ public:
 
 int main(int argc, char* argv[])
 {
-	MPI_Init(&argc, &argv);
-	steady_clock::time_point t1 = steady_clock::now();
+	//MPI_Init(&argc, &argv);
+	//steady_clock::time_point t1 = steady_clock::now();
 	GravimetryTask g("hh1.dat", "hh2.dat");
-	steady_clock::time_point t2 = steady_clock::now();
-	duration<double> time_span = duration_cast<duration<double>>(t2 - t1);
-	cout << "It took me " << time_span.count() << " seconds." << endl;;
-	MPI_Finalize();
+	/*steady_clock::time_point t2 = steady_clock::now();
+	duration<double> time_span = duration_cast<duration<double> >(t2 - t1);
+	cout << "It took me " << time_span.count() << " seconds." << endl;;*/
+	//MPI_Finalize();
 	return 0;
 }
 
